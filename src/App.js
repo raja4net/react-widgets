ebffa1ce7e93e4a3a3dc1e6cc9ecea22d50e3b10
Raja Rupinder Singh <rajarupinder@gmail.com>
2020-08-18 21:49:03 +0530

    Executing request to Wikipedia API from useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Accordion from './components/Accordion';
-
+import Search from './components/Search';
 const items = [
     {
         title: "What is React?",
@@ -20,7 +20,7 @@ const App = () => {
     
     return (
         <div>
-        <Accordion items={items} />
+        <Search />
         </div>);
 
 };
